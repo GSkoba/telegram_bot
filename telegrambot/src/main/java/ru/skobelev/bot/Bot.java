cc4e0570c4b4a91b355d1d9db3c3e0bb062ef719
Grigoriy Skobelev <gskobelevff@gmail.com>
2017-10-25 16:43:24 +0300

    Update Bot.java

--- telegrambot/src/main/java/ru/skobelev/bot/Bot.java
+++ telegrambot/src/main/java/ru/skobelev/bot/Bot.java
@@ -8,12 +8,46 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot{
 
-    @Override
+   @Override
     public void onUpdateReceived(Update e) {
+        Calendar c = Calendar.getInstance();
+        Date d = new Date();
+        c.setTime(d);
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
         Message msg = e.getMessage(); // Это нам понадобится
         String txt = msg.getText();
         if (txt.equals("/start")) {
             sendMsg(msg, "Hello, world! This is simple bot!");
+        }
+        if(txt.equals("raz")){
+            switch (dayOfWeek){
+            case 2:
+                sendMsg(msg,"Теор вер");
+                sendMsg(msg,"ТАУ практика");
+                sendMsg(msg,"ТАУ");
+                sendMsg(msg,"Культурология");
+                break;
+            case 3:
+                sendMsg(msg,"Нет занятий");
+                break;
+            case 4:
+                sendMsg(msg,"Системы");
+                sendMsg(msg,"САИУ");
+                sendMsg(msg,"САИУ");
+                break;
+            case 5:
+                sendMsg(msg,"Базы данных");
+                break;
+            case 6:
+                sendMsg(msg,"Пятница");
+                break;
+            case 7:
+                sendMsg(msg, "Нет пар");
+                break;
+            case 1:
+                sendMsg(msg,"Нет пар");
+                break;
+            }
         }
     }
 
